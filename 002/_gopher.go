bc6868e3334743cb11089cd828a16e6c4d9d225d
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-15 09:32:32 +0900

    add: 002

---
+++ 002/_gopher.go
@@ -0,0 +1,15 @@
+package main
+
+var d int = 1
+
+func gopher() {
+	a := 7
+	b := 3
+
+	c := a + b
+	fmt.Print(c)
+	fmt.Print("Hello")
+
+	Gopher := "Gopher"
+	fmt.Println(Gopher)
+}
---
+++ 002/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"reflect"
+)
+
+func main() {
+	// ファイルごとのトークンの位置を記録するFileSetを作成
+	fset := token.NewFileSet()
+
+	// ファイル単位で構文解析を行う
+	f, err := parser.ParseFile(fset, "_gopher.go", nil, 0)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	log.Println(f)
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		fmt.Println("============")
+		t := reflect.TypeOf(n)
+		fmt.Printf("node: %s\n", n)
+		fmt.Printf("nodeType: %s\n", t)
+		switch node := n.(type) {
+		case *ast.Ident:
+			if node.Name != "Gopher" {
+				return true
+			}
+			fmt.Println((fset.Position(node.Pos())))
+		case *ast.AssignStmt:
+			fmt.Println((fset.Position(node.Pos())))
+		default:
+			return true
+		}
+
+		fmt.Println("============")
+
+		return true
+	})
+}
